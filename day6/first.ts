3ea67f367454271d4bdbabba1210ba891291d5e3
kmlkrdg <kemalkaradag52@gmail.com>
2023-02-15 10:45:04 +0300

    ts

---
+++ day6/first.ts
@@ -0,0 +1,19 @@
+let price: number = 20;
+let price2: number = 20.44;
+let name2: string = "ahmet";
+let isShow: boolean = true;
+let products: null = null;
+let product: undefined = undefined;
+let price3: number = 24;
+price3.toFixed,();
+let price4: unknown = 30;
+let numbers: number[] = [1,2,3,4,5];
+let names: string[] = ["ahmet","mehmet","hasan"];
+let productList: Array<string | number > =["kalem", "defter","silgi",23];
+let value1: string | boolean = true;
+
+//for
+for(let index = 0 ; index < names.length; index++) {
+    const element = names [index]
+    console.log(element);
+}
